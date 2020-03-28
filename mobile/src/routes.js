d9d4f056b719ab48f5fe28cff506e9b92dff5882
Joao <joaopsramos231@gmail.com>
2020-03-28 00:26:11 -0300

    Mobile do app

---
+++ mobile/src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
+
+const AppStack = createStackNavigator()
+
+import Incidents from './pages/Incidents'
+import Detail from './pages/Detail'
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+
+            <AppStack.Navigator
+                screenOptions={({ route, navigation }) => ({
+                    gestureEnabled: true,
+                    ...TransitionPresets.SlideFromRightIOS,
+                })}
+                headerMode="none"
+            >
+                <AppStack.Screen name="Incidents" component={Incidents} />
+                <AppStack.Screen name="Detail" component={Detail} />
+            </AppStack.Navigator>
+
+        </NavigationContainer>
+    )
+}
